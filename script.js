79948ef6927770617bf9ecdb39d255cb1e37d4d3
Prem Mourya <premmourya998@gmail.com>
2024-10-12 22:41:45 +0530

    Home section changed update 4.0

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+let button = document.querySelector('.bars');
+let menu = document.querySelector('ul');
+let btn1  = document.querySelector('#close');
+
+button.addEventListener('click' , () =>{
+    menu.classList.toggle('ShowMenu')
+});
+
+btn1.addEventListener('click' , () =>{
+    menu.classList.toggle('ShowMenu')
+});
+
+
+var swiper = new Swiper('.myswiper' , {
+    slidesPerView:4,
+    spaceBetween:4,
+    autoplay:true,
+    loop:true,
+    breakpoints:{
+        1600:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        1500:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        1400:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        1300:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        1200:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        1000:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        500:{
+            slidesPerView:3,
+            spaceBetween:4,
+        },
+        0:{
+            slidesPerView:2,
+            spaceBetween:4,
+        },
+    },
+});
